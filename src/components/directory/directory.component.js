eb133e9af7017910bd20b3ff17470c6d9ee1c377
Anant Shanbhag <anant.s888@gmail.com>
2021-08-02 23:32:47 +0530

    adding header component and links to home, shop page and contact

--- src/components/directory/directory.component.js
+++ src/components/directory/directory.component.js
@@ -2,6 +2,8 @@ import React from 'react';
 
 import MenuItem from '../menu-item/menu-item.component';
 
+import SECTIONS_DATA from './sections.data';
+
 import "./directory.styles.scss"
 
 class Directory extends React.Component {
@@ -9,40 +11,7 @@ class Directory extends React.Component {
 		super();
 
 		this.state = {
-			sections: [
-				{
-					title: 'hats',
-					imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
-					id: 1,
-					linkUrl: 'hats'
-				},
-				{
-					title: 'jackets',
-					imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
-					id: 2,
-					linkUrl: ''
-				},
-				{
-					title: 'sneakers',
-					imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
-					id: 3,
-					linkUrl: ''
-				},
-				{
-					title: 'womens',
-					imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
-					size: 'large',
-					id: 4,
-					linkUrl: ''
-				},
-				{
-					title: 'mens',
-					imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
-					size: 'large',
-					id: 5,
-					linkUrl: ''
-				}
-			]
+			sections: SECTIONS_DATA
 		}
 	}
 
