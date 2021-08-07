63271ea82c6c985add24d3cd713612001f5615d5
Anant Shanbhag <anant.s888@gmail.com>
2021-08-07 19:36:44 +0530

    updating our app to redirect to home if user is signed in

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import { Route, Switch } from "react-router-dom";
+import { Route, Switch, Redirect } from "react-router-dom";
 import { connect } from 'react-redux';
 
 import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
-import SignInAndSignUp from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 import Header from './components/header/header.component';
 import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 import { setCurrentUser } from './redux/user/user.actions';
@@ -41,21 +41,34 @@ class App extends React.Component {
   }
 
   render() {
+
+    const { currentUser } = this.props;
+
     return (
       <div>
         <Header />
         <Switch>
           <Route exact path="/" component={HomePage} />
           <Route path="/shop" component={ShopPage} />
-          <Route path="/signin" component={SignInAndSignUp} />
+          <Route exact path="/signin" render={() =>
+            currentUser ? <Redirect to='/' />
+              : <SignInAndSignUpPage />
+          } />
         </Switch>
       </div>
     );
   }
 }
 
+const mapStateToProps = ({ user }) => ({
+  currentUser: user.currentUser
+});
+
 const mapDispatchToProps = dispatch => ({
   setCurrentUser: user => dispatch(setCurrentUser(user))
-})
+});
 
-export default connect(null, mapDispatchToProps)(App);
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(App);
--- src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.js
+++ src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.js
@@ -5,11 +5,11 @@ import SignUp from '../../components/sign-up/sign-up.component';
 
 import "./sign-in-and-sign-up.styles.scss"
 
-const SignInAndSignUp = () => (
+const SignInAndSignUpPage = () => (
 	<div className="sign-in-and-sign-up">
 		<SignIn />
 		<SignUp />
 	</div>
 );
 
-export default SignInAndSignUp;
+export default SignInAndSignUpPage;
