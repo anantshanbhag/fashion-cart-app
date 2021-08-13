4aaee938ca0e859c927ebcb15b23a94fd378b278
Anant Shanbhag <anant.s888@gmail.com>
2021-08-13 13:23:07 +0530

    heroku error fix - file name change for git

--- src/pages/homepage/homepage.component.js
+++ src/pages/homepage/homepage.component.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 import Directory from '../../components/directory/directory.component';
 
-import "./homepage.styles.scss"
+import "./homePage.styles.scss"
 
 const HomePage = () => (
 	<div className="homepage">
